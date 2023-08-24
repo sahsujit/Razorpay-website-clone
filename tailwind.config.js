99b45f644a96aaa9c3f9a0950ae2b945d01d7a22
sujit sah <sujit8877123sah@gmail.com>
2023-08-24 11:35:19 +0545

    Razorpay

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["*"],
   theme: {
     extend: {
+      fontFamily:{
+        mulish:["mulish", "sans-serif"]
+      },
 
       colors: {
         deepBlue: "#02042a",
